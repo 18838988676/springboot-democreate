2f163d369b3adaa51a3a5abf664f74762c90832b
18838988676 <18838988676@163.com>
2018-10-18 19:24:10 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/SpringbootDemocreateApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemocreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemocreateApplication.class, args);
+	}
+}
