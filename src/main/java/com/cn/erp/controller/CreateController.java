7154b4aa31bcfe2c8a950c7c75ce4c2493ad3a87
18838988676 <18838988676@163.com>
2018-10-19 11:49:28 +0800

    代码生成器正常运行

---
+++ src/main/java/com/cn/erp/controller/CreateController.java
@@ -0,0 +1,69 @@
+package com.cn.erp.controller;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cn.erp.service.SysGeneratorService;
+import com.cn.erp.untils.PageUtils;
+import com.cn.erp.untils.Query;
+import com.cn.erp.untils.R;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/sys/generator")
+public class CreateController {
+	@Autowired
+	private SysGeneratorService sysGeneratorService;
+	
+	@RequestMapping("/ge")
+	public String testN()  {
+		System.out.println("da");
+		return "/index.html";
+	}
+	
+	
+	/**
+	 * 列表
+	 */
+	@ResponseBody
+	@RequestMapping("/list")
+	public R list(@RequestParam Map<String, Object> params){
+		//查询列表数据
+		Query query = new Query(params);
+		List<Map<String, Object>> list = sysGeneratorService.queryList(query);
+		int total = sysGeneratorService.queryTotal(query);
+		
+		PageUtils pageUtil = new PageUtils(list, total, query.getLimit(), query.getPage());
+		
+		return R.ok().put("page", pageUtil);
+	}
+
+	/**
+	 * 生成代码
+	 */
+	@RequestMapping("/code")
+	public void code(String tables, HttpServletResponse response) throws IOException{
+		byte[] data = sysGeneratorService.generatorCode(tables.split(","));
+
+		response.reset();
+		response.setHeader("Content-Disposition", "attachment; filename=\"renren.zip\"");
+		response.addHeader("Content-Length", "" + data.length);
+		response.setContentType("application/octet-stream; charset=UTF-8");
+
+		IOUtils.write(data, response.getOutputStream());
+	}
+}
---
+++ src/main/resources/readme.txt
@@ -0,0 +1,1 @@
+启动程序后：http://localhost:8088/ 
